1323fceb6f011636a1f31e41ae32c1fc0091456c
Christopher <leechrs1@gmail.com>
2022-07-24 11:10:28 -0500

    still don't understand the logic to get the page to render conitionally

--- src/App.js
+++ src/App.js
@@ -11,30 +11,26 @@ import Footer from './components/Footer';
 function App() {
 
   const [contactSelected, setContactSelected] = useState(false);
-  const [resumeSelected, setResumeSelected] = useState(false);
-  const [portfolioSelected, setPortfolioSelected] = useState(false);
+
+  // AskBCS said I don't need to set the below useStates to false
+  // const [resumeSelected, setResumeSelected] = useState(false);
+  // const [portfolioSelected, setPortfolioSelected] = useState(false);
 
    return (
     <div>
       <Nav
         contactSelected={contactSelected}
         setContactSelected={setContactSelected}
-        resumeSelected={resumeSelected}
-        setResumeSelected={setResumeSelected}
-        portfolioSelected={portfolioSelected}
-        setPortfolioSelected={setPortfolioSelected}
+        // resumeSelected={resumeSelected}
+        // setResumeSelected={setResumeSelected}
+        // portfolioSelected={portfolioSelected}
+        // setPortfolioSelected={setPortfolioSelected}
       >
       </Nav>
       <main>
-        {!contactSelected ? (
+        {
           <About></About>
-        ) : (
-          <>
-          <ContactForm></ContactForm>
-          <Resume></Resume>
-          <Portfolio></Portfolio>
-          </>
-        )}
+        }
       </main>
       <Footer></Footer>
     </div>
@@ -42,3 +38,15 @@ function App() {
 }
 
 export default App;
+
+// Moved the original code to the bottom to try and get the page to render properly
+
+// {!contactSelected ? (
+//   <About></About>
+// ) : (
+//   <>
+//   <ContactForm></ContactForm>
+//   <Resume></Resume>
+//   <Portfolio></Portfolio>
+//   </>
+// )}
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,6 +12,10 @@ function Footer() {
             <div>
                 <a href="https://www.linkedin.com/in/christopher-lee-08a6ab7/" className="logoImages"><img src={require("../../assets/logo/LI-In-Bug.png")} width={50} height={50} alt="LinkedIn"></img></a>
             </div>
+
+            <div>
+            <a href="https://www.linkedin.com/in/christopher-lee-08a6ab7/" className="logoImages"><img src={require("../../assets/logo/twitter2021.png")} width={50} height={50} border-radius={50} alt="LinkedIn"></img></a>
+            </div>
         </footer>
     );
 }
