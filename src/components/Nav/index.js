cd60a9e31a9e0a3b02997f9c0e28f9feea72aca0
Christopher <leechrs1@gmail.com>
2022-07-24 20:53:19 -0500

    added resume for download and changed color scheme again

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -12,19 +12,25 @@ function Nav({ currentPage, handlePageChange}) {
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="about" onClick={() => handlePageChange('About')}>About me</a>
+                        <a href="about" onClick={() => handlePageChange('About')}
+                        className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                        >About me</a>
                     </li> 
 
                     <li className="mx-2">
-                        <span onClick={() => handlePageChange('Contact')}>Contact</span>  
+                        <span onClick={() => handlePageChange('Contact')}
+                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</span>  
                     </li>
 
                     <li className="mx-2">
-                        <span onClick={() => handlePageChange('Resume')}>Resume</span>
+                        <span onClick={() => handlePageChange('Resume')}
+                        className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                        >Resume</span>
                     </li>
 
                     <li className="mx-2">
-                        <span onClick={() => handlePageChange('Portfolio')}>Portfolio</span>
+                        <span onClick={() => handlePageChange('Portfolio')}
+                        className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</span>
                     </li>
                 </ul>
             </nav>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,14 +7,21 @@ function Resume() {
         <section className="my-5">
             <h2>Resume</h2>
 
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tellus purus, commodo vel dolor id, ullamcorper viverra quam. Ut non finibus lectus, ut porttitor velit. Integer sapien felis, egestas ut massa ut, imperdiet mollis ligula. Curabitur nec odio dapibus arcu finibus suscipit. Aliquam luctus mauris quam, quis gravida mi venenatis non. Proin ullamcorper feugiat lectus eu pretium. Aenean quis odio risus. Mauris vel velit blandit, bibendum lacus in, fringilla risus. Mauris rutrum lacinia fringilla. Aenean bibendum erat ultrices, accumsan lorem et, accumsan lacus.
-            </p>
-
-            <p>
-            Praesent ut felis tristique, luctus lorem ut, placerat tellus. Sed neque sem, laoreet vel laoreet in, pulvinar vitae purus. Aliquam at rutrum justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. In rutrum quis tortor in dignissim. Maecenas rutrum posuere lorem non dictum. Maecenas nec nulla lorem. Aenean aliquet, ipsum ac facilisis venenatis, sapien lorem consequat massa, non blandit eros ligula posuere purus. Quisque sollicitudin nulla elit, at vulputate tortor sollicitudin a. Morbi elementum nunc fermentum, consectetur ante eget, interdum enim. Duis eget tempor leo, vitae sodales enim.
-            </p>
-        </section>
+            <h4>Proficiencies</h4>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Mobile-first responsive web design</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>React</li>
+            </ul>
+            <div>
+                Download my resume 
+                <a href={require("../../assets/2021_Resume.pdf")} download className="download">here</a>
+            </div>
+        </section>  
     )
 }
 
